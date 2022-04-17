13539680e2db9cb942c84c687a91b691ed04e07d
Kimhanyeol <khy000426@naver.com>
2022-04-18 01:00:05 +0900

    메인 액티비티에 로그아웃&계정탈퇴&지도보기 버튼 생성

--- app/src/main/java/com/example/namecard/MainActivity.java
+++ app/src/main/java/com/example/namecard/MainActivity.java
@@ -2,13 +2,54 @@ package com.example.namecard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_logout =findViewById(R.id.btn_logout);
+        Button btn_delete_account = findViewById(R.id.btn_delete_account);
+        Button btn_map = findViewById(R.id.btn_map);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //로그아웃
+                mFirebaseAuth.signOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btn_delete_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mFirebaseAuth.getCurrentUser().delete();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btn_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
